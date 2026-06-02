ff3ea4a9f23dfa4c19e9e94f65e505e5248e2d5c
yatfu <55002354+yatfu@users.noreply.github.com>
2026-06-01 19:05:30 -1000

    removed prisma
    having too many issues with prisma. apparently its unstable with my current configuration and working around these issues is making my head hurt. also cleared some missed mongodb files. switching to regular postgres

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,9 +1,14 @@
-
+"use client";
+import { useState } from "react";
 
 function Nav() {
+  const [currentTab, setCurrentTab] = useState("month");
+  
   return (
     <div className="nav py-3 flex justify-center">
       <button className="">Sign In not implemented</button>
+      <button onClick={() => setCurrentTab("month")}>Monthly</button>
+      <button onClick={() => setCurrentTab("records")}>Yearly</button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,17 @@
-"use client";
-import { useState } from "react";
+
 import Nav from './components/Nav'
 import Expenses from './components/Expenses'
-import Income from './components/Income'
-import Ratio from './components/Ratio'
 
 function App() {
   /* page has two tabs
        one to crud data of budget per month
        one to display data of all months */
-  const [currentTab, setCurrentTab] = useState("month");
 
   return (
     <>
       <Nav />
-      <button onClick={() => setCurrentTab("month")}>Monthly</button>
-      <button onClick={() => setCurrentTab("records")}>Yearly</button>
       <div className="">
-        {currentTab === "month" && 
-          <div>
-            <h2>Expenses</h2>
-            <Expenses />
-            <h2>Income</h2>
-            <Income />
-        </div>
-        }
-        {currentTab === "records" && <div className=""><Ratio /></div>}
+        <Expenses />
       </div>
       
     </>
