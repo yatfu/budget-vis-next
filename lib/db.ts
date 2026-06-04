5d43a8fca57bfbfd76b9716a7fbfb74ed37cd689
yatfu <55002354+yatfu@users.noreply.github.com>
2026-06-04 11:29:44 -1000

    initialized pg with connections, added files for organizing queries
    queries are organized by databases in sql

---
+++ lib/db.ts
@@ -0,0 +1,28 @@
+import 'server-only'
+import { Pool } from "pg";
+
+/** 
+ *keep singleton pool to prevent too many connections from nextjs dev mode
+ */
+
+declare global { // so typescript doesnt complain
+  var pgPool: Pool | undefined;
+}
+
+const connectionString = process.env.DATABASE_URL; // ChatGPT recommended to check env variable because pg errors aren't obvious
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export const pool =
+  global.pgPool ??
+  new Pool({
+    connectionString,
+    max: 10, // concurrent connection max
+    idleTimeoutMillis: 50_000,
+  });
+
+if (process.env.NODE_ENV !== "production") { // only need this feature in dev mode
+  global.pgPool = pool;
+}
+
