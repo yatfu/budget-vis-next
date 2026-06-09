d64b723442d7f1d6a1fdbde2c29414347823e9d3
yatfu <55002354+yatfu@users.noreply.github.com>
2026-06-09 13:38:22 -1000

    added expenses get api endpoint
    gets all expenses with given user_id

--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -1,6 +1,20 @@
 import { pool } from '@/db/db'
 
 export async function GET(req: Request) {
-  const result = await pool.query("SELECT * FROM expenses");
-  return Response.json(result.rows);
+  const { searchParams } = new URL(req.url); // parses url into URL format to extract id
+  const userId = searchParams.get("userId");
+
+  if (!userId) { // error handling
+    return Response.json(
+      { error: "userId is required" },
+      { status: 400 }
+    );
+  }
+
+  const result = await pool.query( // result returns database result object
+    "SELECT * FROM expenses WHERE user_id = $1",
+    [userId]
+  );
+
+  return Response.json(result.rows); // converts result into json, then sends it as http response
 }
