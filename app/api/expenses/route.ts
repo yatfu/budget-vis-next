ef4852a8da7058357fa376a83f6e82524c60539e
yatfu <55002354+yatfu@users.noreply.github.com>
2026-06-09 13:50:16 -1000

    added datatype check for user_id

--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -4,17 +4,26 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url); // parses url into URL format to extract id
   const userId = searchParams.get("userId");
 
-  if (!userId) { // error handling
+  if (!userId) { // error handling: null
     return Response.json(
       { error: "userId is required" },
       { status: 400 }
     );
   }
 
+  const parsedUserId = Number(userId) // error handling: is integer
+  if (!Number.isInteger(parsedUserId)) {
+    return Response.json(
+      { error: "user_id not correct datatype"},
+      { status: 400 }
+    )
+  }
+
   const result = await pool.query( // result returns database result object
     "SELECT * FROM expenses WHERE user_id = $1",
-    [userId]
-  );
+    [parsedUserId]) // used parsed user id instead of original because ChatGPT recommended to. SQL will auto-parse strings, and with the integer check it should be fine with original but reduces reliability by depending on SQL auto parse
+
+
 
   return Response.json(result.rows); // converts result into json, then sends it as http response
 }
