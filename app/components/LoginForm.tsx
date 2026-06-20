79bc13f4c55f325258b666e8060d04adfb585a2b
yatfu <55002354+yatfu@users.noreply.github.com>
2026-06-19 14:53:16 -1000

    created login form

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState, type FormEvent } from "react"; // form event is only used for typescript declaration
+
+type LoginFormProps ={
+  onSubmit?: (username: string, password: string) => void;
+}
+
+export default function LoginForm({ onSubmit }: LoginFormProps) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (onSubmit) {
+      onSubmit(username, password);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <div>
+
+        <label>Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+      </div>
+
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
