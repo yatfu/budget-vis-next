e99b16e1c027905fb40b14fc40ea9b84ffa2593c
yatfu <55002354+yatfu@users.noreply.github.com>
2026-07-06 23:16:23 -1000

    feat: sort buttons, amount and label

--- app/components/Expenses.jsx
+++ app/components/Expenses.jsx
@@ -3,7 +3,7 @@
 import ExpensesForm from "./ExpensesForm";
 import ExpensesChart from "./ExpensesChart";
 import { useState, useEffect } from "react";
-import DateSelector from './DateSelector';
+import DateSelector from "./DateSelector";
 
 const Expenses = () => {
   const now = new Date();
@@ -11,31 +11,37 @@ const Expenses = () => {
   const [expenses, setExpenses] = useState([]);
   const [selectedMonth, setSelectedMonth] = useState(now.getMonth() + 1); // 1 == january
   const [selectedYear, setSelectedYear] = useState(now.getFullYear());
+  const [sortBy, setSortBy] = useState("none"); // "amount" | "name"
 
   useEffect(() => {
     (async () => {
-      const cachedExpenses = localStorage.getItem("expenses");
-      if (cachedExpenses) {
-        setExpenses(JSON.parse(cachedExpenses));
-        console.log("Found expenses cached in localstorage")
-        return;
-      }
       const res = await fetch(`/api/expenses`);
       const data = await res.json();
       console.log("EXPENSES DATA:", data);
       setExpenses(data);
-      localStorage.setItem("expenses", JSON.stringify(data));
     })();
   }, []);
 
   const filteredExpenses = expenses
     .filter((expense) => expense.month === selectedMonth)
-    .filter((expense) => expense.year === selectedYear); // used for rendering selected dates
+    .filter((expense) => expense.year === selectedYear) // used for rendering selected dates
+    .slice() // copy
+    .sort((a, b) => {
+      if (sortBy === "amount") {
+        return b.amount - a.amount;
+      } 
+      else if (sortBy === "label") {
+        return a.label.localeCompare(b.label);
+      }
+      else {
+        return 0; // means dont sort
+      }
+    });
 
   //<ExpensesForm expenses={expenses} setExpenses={setExpenses} />
   return (
     <div className="expenses">
-      <DateSelector 
+      <DateSelector
         selectedMonth={selectedMonth}
         setSelectedMonth={setSelectedMonth}
         selectedYear={selectedYear}
@@ -46,7 +52,27 @@ const Expenses = () => {
         labels={filteredExpenses.map((expense) => expense.label)}
         values={filteredExpenses.map((expense) => parseFloat(expense.amount))}
       />
-      <ExpensesForm expenses={expenses} setExpenses={setExpenses} selectedMonth={selectedMonth} selectedYear={selectedYear} />
+      <button
+        onClick={() => {
+          setSortBy("label");
+        }}
+      >
+        Name
+      </button>
+      <button
+        onClick={() => {
+          setSortBy("amount");
+        }}
+      >
+        Amount
+      </button>
+
+      <ExpensesForm
+        expenses={filteredExpenses}
+        setExpenses={setExpenses}
+        selectedMonth={selectedMonth}
+        selectedYear={selectedYear}
+      />
     </div>
   );
 };
