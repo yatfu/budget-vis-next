2beeb04e178f237d9598790bc8b52752504b48e0
yatfu <55002354+yatfu@users.noreply.github.com>
2026-07-08 07:47:06 -1000

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) { // shadcn helper
   return twMerge(clsx(inputs))
 }
 
