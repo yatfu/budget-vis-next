1980915f5f2563944b4edeac35b85e3ce01708ca
yatfu <55002354+yatfu@users.noreply.github.com>
2026-07-08 02:53:17 -1000

    refactor: broke down expensesForm into series of expenseRows, which has expenses mapped to it

--- app/components/ExpenseRow.tsx
+++ app/components/ExpenseRow.tsx
@@ -15,48 +15,47 @@ export default function ExpenseRow({
     setAmount(0);
   };
   return (
-    <>
-      <div key={expense.id}>
-        <button
-          type="button"
-          onClick={() => {
-            onDeleteExpense(index);
-          }}
-        >
-          X
-        </button>
-        <input
-          type="text"
-          placeholder="Expense Name"
-          value={expense.label}
-          onChange={(e) => onChangeExpense(index, "label", e.target.value)}
-          required
-        />
-        <input
-          type="number"
-          min="0"
-          step="0.01"
-          placeholder="Value"
-          value={expense.amount}
-          onChange={(e) => {
-            const parsed = parseFloat(e.target.value);
-           onChangeExpense(index, "amount", isNaN(parsed) ? 0 : parsed); // CHATGPT solution to bug: when deleting the entire input, NaN is given to parseFloat which causes error
-          }}
-          required
-        />
-      </div>
-      <input type="number" 
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          onDeleteExpense(index);
+        }}
+      >
+        X
+      </button>
+      <input
+        type="text"
+        placeholder="Expense Name"
+        value={expense.label}
+        onChange={(e) => onChangeExpense(index, "label", e.target.value)}
+        required
+      />
+      <input
+        type="number"
+        min="0"
+        step="0.01"
+        placeholder="Value"
+        value={expense.amount}
         onChange={(e) => {
-          setAmount(parseFloat(e.target.value))
+          const parsed = parseFloat(e.target.value);
+          onChangeExpense(index, "amount", isNaN(parsed) ? 0 : parsed); // CHATGPT solution to bug: when deleting the entire input, NaN is given to parseFloat which causes error
+        }}
+        required
+      />
+      <input
+        type="number"
+        onChange={(e) => {
+          setAmount(parseFloat(e.target.value));
         }}
         placeholder="Add"
-        value={amount} 
-        />
+        value={amount}
+      />
       <button type="button" onClick={() => handleAdd(index, amount)}>
         {" "}
         +{" "}
       </button>
-    </>
+    </div>
   );
 }
 /**
